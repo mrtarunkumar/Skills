2f3276fdb3b99521e378a1cbba3b655443c47d37
unknown <tarunkumarv@8389VM.charnwood.local>
2018-06-18 11:43:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Skills/>
+  </div>
+</template>
+
+<script>
+import Skills from './components/Skills'
+
+export default {
+  name: 'App',
+  components: {
+    Skills
+  }
+}
+</script>
+
+<style scoped>
+  body{
+    background-color: #eeeeee
+  }
+</style>
