c616a6aec249c560b570cc59e239f8b3580e940f
mrtarunkumar <tarunkumar.vegamwala@ntlworld.com>
2018-06-19 10:35:23 +0100

    Update and new files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <Skills/>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+    <router-view/>
   </div>
 </template>
 
@@ -16,7 +20,38 @@ export default {
 </script>
 
 <style scoped>
-  body{
-    background-color: #eeeeee
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+  body {
+    background-color: #EEEEEE;
+    font-family: 'Montserrat', sans-serif;
+    display: grid;
+    grid-template-rows: auto;
+    justify-items: center;
+    padding-top: 50px;
+    align-items: center;
+  }
+  body, html {
+    margin: 0;
+    height: 100%;
+  }
+  #app {
+      width: 50%;
+      margin-left: auto;
+      margin-right: auto;
+  }
+
+  nav {
+    padding: 20px 20px 20px 0;
+  }
+
+  nav a {
+    padding: 10px;
+    text-decoration: none;
+    background: #fff;
+    border-radius: 3px;
+    color: rgb(0, 110, 255);
+    font-weight: bold;
+    margin-right: 15px;
   }
 </style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="skills">
-    <h1 v-once>{{name}}</h1>
-    {{ btnState ? 'The button is disabled' : 'The button is active'}}
-    <button v-on:click="changeName" v-bind:disable="btnState">Change Name</button>
-    <ul>
-      <li v-for="(data, index) in skills" :key="index">{{index}} {{data.skill}}</li>
-    </ul>
-      <p v-if="skills.length >= 1">You have more than 1 skill</p>
-      <p v-else>You have less than or equal to 1 skill</p>
-      <!-- <div v-bind:class="{ 'alert': showAlert, 'anotherClass': showClass}"></div> -->
-      <div v-bind:class="alertObj"></div>
-      <!-- <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight, border: bdColor }"></div> -->
-      <div v-bind:style="alertObjTwo"></div>
+  <div class="container">
+    <form @submit.prevent="addSkill">
+      <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
+      <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+      </transition>
+      <!-- <input type="checkbox" id="checkbox" v-model="checked"> -->
+      <!-- { skill }} -->
+    </form>
+
+    <div class="holder">
+      <ul>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+          <li v-for="(data, index) in skills" :key='index'>{{data.skill}}
+            <i class="fai fa fa-minus-circle" v-on:click="remove(index)"></i>
+          </li>
+        </transition-group>
+      </ul>
+      <p>These are the skills that you possess.</p>
+    </div>
   </div>
 </template>
 
@@ -23,39 +30,106 @@ export default {
   data () {
     return {
       name: 'Skills Course',
-      btnState: true,
-      showAlert: true,
-      showClass: true,
+      checked: false,
+      skill: '',
       skills: [
-        {'skill': 'vuejs'},
-        {'skill': 'front end developer'}
-      ],
-      alertObj: {
-        alert: true,
-        anotherClass: true
-      },
-      bgColor: 'orange',
-      bgWidth: '100%',
-      bgHeight: '30px',
-      bdColor: '1px solid red',
-      alertObjTwo: {
-        backgroundColor: 'green',
-        width: '100%',
-        height: '30px',
-        border: '1px solid red'
-      }
+        {'skill': 'Vuejs'},
+        {'skill': 'Front end developer'}
+      ]
+    }
+  },
+  methods: {
+    addSkill () {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.skills.push({skill: this.skill})
+          this.skill = ''
+        } else {
+          console.log('Not valid')
+        }
+      })
+    },
+    remove (id) {
+      this.skills.splice(id, 1)
     }
   }
 }
 </script>
 
 <style scoped> /* external file <style src="./Skills.css">  */
-  .alert{
-    background-color: yellow;
-    width:100%;
-    height: 30px;
+  @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+  @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+
+  .holder {
+    background: #fff;
   }
-  .anotherClass{
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+  .fai{
+    float:right;
+    width: 10px;
+    height: 10px;
     border: 1px solid red;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 </style>
